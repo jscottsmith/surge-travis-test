845ada41417e237a5148c377124b9abb60bcb90b
jscottsmith <jscsmith@gmail.com>
2018-10-23 16:05:07 -0700

    ad develop url

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ REPO_SLUG_ARRAY=(${TRAVIS_REPO_SLUG//\// })
 REPO_OWNER=${REPO_SLUG_ARRAY[0]}
 REPO_NAME=${REPO_SLUG_ARRAY[1]}
 DEPLOY_PATH=./build
+PROD_SURGE_DOMAIN=https://gumdrops.surge.sh
+DEV_SURGE_DOMAIN=https://gumdrops-develop.surge.sh
 
 DEPLOYMENT_BEGIN="== Running deploy script for $REPO_NAME by ${REPO_OWNER} =="
 echo $DEPLOYMENT_BEGIN
@@ -12,25 +14,9 @@ DEPLOY_SUBDOMAIN_UNFORMATTED_LIST=()
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
   DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_PULL_REQUEST}-pr)
-elif [ -n "${TRAVIS_TAG// }" ] #TAG is not empty
-then
-  #sorts the tags and picks the latest
-  #sort -V does not work on the travis machine
-  #sort -V              ref: http://stackoverflow.com/a/14273595/689223
-  #sort -t ...          ref: http://stackoverflow.com/a/4495368/689223
-  #reverse with sed     ref: http://stackoverflow.com/a/744093/689223
-  #git tags | ignore release candidates | sort versions | reverse | pick first line
-  LATEST_TAG=`git tag | grep -v rc | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | sed '1!G;h;$!d' | sed -n 1p`
-  echo $LATEST_TAG
-  if [ "$TRAVIS_TAG" == "$LATEST_TAG" ]
-  then
-    DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(latest)
-  fi
-  DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_TAG}-tag)
 else
   DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_BRANCH})
 fi
-
 
 for DEPLOY_SUBDOMAIN_UNFORMATTED in "${DEPLOY_SUBDOMAIN_UNFORMATTED_LIST[@]}"
 do
@@ -42,23 +28,30 @@ do
   # https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
   DEPLOY_SUBDOMAIN=`echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | sed -r 's/[\/|\.]+/\-/g'`
 
-  # deploy to master domain else custom domain
+  # set master/develop domain else custom domain
   if [ "$TRAVIS_BRANCH" == "master" ]
   then
-    DEPLOY_DOMAIN=https://gumdrops.surge.sh
+    DEPLOY_DOMAIN=PROD_SURGE_DOMAIN
+  if [ "$TRAVIS_BRANCH" == "develop" ]
+  then
+    DEPLOY_DOMAIN=DEV_SURGE_DOMAIN
   else
     DEPLOY_DOMAIN=https://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.surge.sh
   fi
+
+  # send to surge
   surge --project ${DEPLOY_PATH} --domain $DEPLOY_DOMAIN;
 
-  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-  then
-    # Using the Issues api instead of the PR api
-    # Done so because every PR is an issue, and the issues api allows to post general comments,
-    # while the PR api requires that comments are made to specific files and specific commits
-    GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
-    curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment: '${DEPLOY_DOMAIN}'"}'
-  fi
+  # Enable for PR comments, requires token to be set.
+
+  # if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+  # then
+  #   # Using the Issues api instead of the PR api
+  #   # Done so because every PR is an issue, and the issues api allows to post general comments,
+  #   # while the PR api requires that comments are made to specific files and specific commits
+  #   GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
+  #   curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment: '${DEPLOY_DOMAIN}'"}'
+  # fi
 done
 
 exit 0

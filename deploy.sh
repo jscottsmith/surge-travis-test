e1b6877a410e520560bd7bcb523465be4607fbd2
jscottsmith <jscsmith@gmail.com>
2018-10-23 12:41:47 -0700

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,21 @@ DEPLOY_SUBDOMAIN_UNFORMATTED_LIST=()
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
   DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_PULL_REQUEST}-pr)
+elif [ -n "${TRAVIS_TAG// }" ] #TAG is not empty
+then
+  #sorts the tags and picks the latest
+  #sort -V does not work on the travis machine
+  #sort -V              ref: http://stackoverflow.com/a/14273595/689223
+  #sort -t ...          ref: http://stackoverflow.com/a/4495368/689223
+  #reverse with sed     ref: http://stackoverflow.com/a/744093/689223
+  #git tags | ignore release candidates | sort versions | reverse | pick first line
+  LATEST_TAG=`git tag | grep -v rc | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | sed '1!G;h;$!d' | sed -n 1p`
+  echo $LATEST_TAG
+  if [ "$TRAVIS_TAG" == "$LATEST_TAG" ]
+  then
+    DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(latest)
+  fi
+  DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_TAG}-tag)
 else
   DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_BRANCH}-branch)
 fi
